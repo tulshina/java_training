c728689669b8b1138592ec6f05e1c813e84f2928
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-02-19 15:58:03 +0300

    Lesson2
    Test case for Square

---
+++ sandbox/src/test/java/ru/stqa/SquareTests.java
@@ -0,0 +1,16 @@
+package ru.stqa;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by User on 19.02.2017.
+ */
+public class SquareTests {
+
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25);
+    }
+}
