7d52a47bd4abca1a4701b8037b625d9b59fb3b8f
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-03-25 14:07:58 +0300

    Лекция #3. Оптимизация заполнения форм

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/HelperBase.java
@@ -21,10 +21,16 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
     }
-    public  boolean isAlertPresent() {
+
+    public boolean isAlertPresent() {
         try {
             wd.switchTo().alert();
             return true;
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -10,7 +10,7 @@ import ru.stqa.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {
