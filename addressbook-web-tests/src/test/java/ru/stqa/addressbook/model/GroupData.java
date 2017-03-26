6ffa017599b812b14ea3411eebf9ff5ed4403114
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-03-26 19:03:16 +0300

    Лекция #4. Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/GroupData.java
@@ -12,10 +12,25 @@ public class GroupData {
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 
     public GroupData(int id, String name, String header, String footer) {
@@ -49,21 +64,4 @@ public class GroupData {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != groupData.id) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
 }
