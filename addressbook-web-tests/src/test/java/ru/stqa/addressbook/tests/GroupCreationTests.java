6f2ba03f4c4f991cc10d0ea6d3e76edf2472dcf5
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-03-26 16:18:22 +0300

    Лекция #4. Проверка количества групп на странице

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,18 @@
 package ru.stqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 
-public class GroupCreationTests extends TestBase  {
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
--- sandbox/src/main/java/ru/stqa/Collections.java
+++ sandbox/src/main/java/ru/stqa/Collections.java
@@ -1,5 +1,9 @@
 package ru.stqa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by User on 26.03.2017.
  */
@@ -8,8 +12,13 @@ public class Collections {
     public static void main (String[] args) {
         String[] langs = {"Java", "C#", "Python", "PHP"};
 
-        for (int i = 0; i < langs.length; i++) {
-            System.out.println("Я хочу выучить" + langs[i]);
+//        List<String> languages = new ArrayList<String>();
+//        languages.add("Java");
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
         }
 
     }
