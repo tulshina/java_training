c849486f63b8dc03aa1c261238c0300965e786f0
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-04-10 00:01:48 +0300

    Лекция #6. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,14 +1,24 @@
 package ru.stqa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by User on 26.02.2017.
  */
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
         protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
@@ -19,6 +29,16 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public  void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 
 }
