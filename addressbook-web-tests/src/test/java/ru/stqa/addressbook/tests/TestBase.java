26fc9efea78748bc013b90c06add32af369a8013
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-04-15 16:26:28 +0300

    Лекция #7. Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.stqa.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +10,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.addressbook.appmanager.ApplicationManager;
+import ru.stqa.addressbook.model.GroupData;
+import ru.stqa.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by User on 26.02.2017.
@@ -19,7 +27,7 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-        protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
@@ -37,8 +45,17 @@ public class TestBase {
     }
 
     @AfterMethod(alwaysRun = true)
-    public  void logTestStop(Method m) {
+    public void logTestStop(Method m) {
         logger.info("Stop test " + m.getName());
     }
 
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
 }
