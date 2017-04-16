5a2848812b0d64a9394275af7635f88257625de3
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-04-16 19:48:37 +0300

    Лекция #9.  SOAP: простой пример

---
+++ soap-sample/src/test/java/ru/stqa/soap/GeoIpServiceTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by User on 16.04.2017.
+ */
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("109.172.15.13");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+
+    @Test
+    public void testInvalidIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("109.172.15.xx");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+}
