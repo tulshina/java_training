fe496e2a92bb1a5706ce998c4c971ded77e4f9ad
Tatiana Ulshina <yunchenkotanya@gmail.com>
2017-04-19 00:27:04 +0300

    expected result was changed

--- sandbox/src/test/java/ru/stqa/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/SquareTests.java
@@ -10,7 +10,7 @@ public class SquareTests {
 
     @Test
     public void testArea() {
-        Square s = new Square(5);
-        Assert.assertEquals(s.area(), 25);
+        Square s = new Square(5.0);
+        Assert.assertEquals(s.area(), 25.0);
     }
 }
